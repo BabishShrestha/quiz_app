dee28eb13b92a253a87afc8b82d8891c85a698c4
BabishShrestha <babishshrestha8@gmail.com>
2022-04-12 14:38:37 +0545

    Renamed MyApp to QuizApp and removed debugBanner.

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,20 @@ import 'presentation/pages/home_page.dart';
 void main() {
   runApp(
     const ProviderScope(
-      child: MyApp(),
+      child: QuizApp(),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class QuizApp extends StatelessWidget {
+  const QuizApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Quiz App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'Poppins',
         primarySwatch: Colors.blue,
