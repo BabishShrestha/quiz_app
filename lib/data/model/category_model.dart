307222491487341d73099ba721534e5000c45155
BabishShrestha <babishshrestha8@gmail.com>
2022-04-17 20:48:20 +0545

    Created a model and repository directory, inside data dir. Added category model in model dir that assigns id and name to the category model

---
+++ lib/data/model/category_model.dart
@@ -0,0 +1,16 @@
+import 'package:quiz_app/domain/entities/category.dart';
+
+class CategoryModel extends Quiz_Category {
+  CategoryModel({required int id, required String name})
+      : super(id: id, name: name);
+
+  factory CategoryModel.fromJson(Map<String, dynamic> json) {
+    return CategoryModel(id: json['id'], name: json['name']);
+  }
+  static List<Quiz_Category> fromData(
+      List<Map<String, dynamic>> category_pool) {
+    return category_pool
+        .map((category) => CategoryModel.fromJson(category))
+        .toList();
+  }
+}
