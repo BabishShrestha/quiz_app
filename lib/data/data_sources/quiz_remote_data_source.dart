60eb84202236a002621ae7537b01e77517164391
BabishShrestha <babishshrestha8@gmail.com>
2022-04-17 20:47:09 +0545

    Created a data directory and added quiz_remote_data_source.dart to fetch category from api by creating an abstract class and implementing it with the help of http

---
+++ lib/data/data_sources/quiz_remote_data_source.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:quiz_app/data/model/category_model.dart';
+import 'package:quiz_app/domain/entities/category.dart';
+
+abstract class CategoryRemoteDataSource {
+  /// Calls the https://opentdb.com/api_category.php endpoint.
+  ///
+  /// Throws a [ServerException] for all error codes.
+  Future<List<Quiz_Category>> getCategoryItem();
+}
+
+class CategoryRemoteDataSourceImpl extends CategoryRemoteDataSource {
+  final http.Client client;
+
+  CategoryRemoteDataSourceImpl({required this.client});
+  @override
+  Future<List<Quiz_Category>> getCategoryItem() {
+    return _getCategoryFromURL("https://opentdb.com/api_category.php");
+  }
+
+  Future<List<Quiz_Category>> _getCategoryFromURL(String url) async {
+    final response = await client.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      List<Map<String, dynamic>> category_pool =
+          List<Map<String, dynamic>>.from(
+              jsonDecode(response.body)['trivia_categories']);
+
+      return CategoryModel.fromData(category_pool);
+    } else {
+      throw UnimplementedError("Not loaded");
+    }
+  }
+}
