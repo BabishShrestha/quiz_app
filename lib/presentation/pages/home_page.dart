34c33565acb25ed65f9fd9885d41e16c9582acfe
BabishShrestha <babishshrestha8@gmail.com>
2022-04-18 19:08:42 +0545

    Added a Future Builder to display the categories remotely, creating 3 conditions, when the snapshot data is loading, when data has error, and when snapshot data has retrieved the data. Changed the getCategory() function from void to Future as it is used to return future value for the future builder

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,34 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:lottie/lottie.dart';
 import 'package:quiz_app/domain/entities/category.dart';
 import 'package:quiz_app/presentation/widgets/widgets.dart';
 
+import '../../constants.dart';
 import '../../data/data_sources/quiz_remote_data_source.dart';
 
-class QuizHomePage extends StatefulWidget {
+class QuizHomePage extends StatelessWidget {
   const QuizHomePage({Key? key}) : super(key: key);
-  @override
-  State<QuizHomePage> createState() => _QuizHomePageState();
-}
 
-class _QuizHomePageState extends State<QuizHomePage> {
-  @override
-  void initState() {
-    super.initState();
-    quizget();
-  }
-
-  void quizget() async {
+  Future<List<Quiz_Category>> getCategory() async {
     CategoryRemoteDataSourceImpl categoryRemoteDataSourceImpl =
         CategoryRemoteDataSourceImpl(client: http.Client());
     List<Quiz_Category> categories =
         await categoryRemoteDataSourceImpl.getCategoryItem();
-    print(await categories.toString());
+    return categories;
   }
 
   @override
   Widget build(BuildContext context) {
-    quizget();
     return Scaffold(
       bottomNavigationBar: const BottomNavBar(),
       body: SafeArea(
@@ -38,7 +29,22 @@ class _QuizHomePageState extends State<QuizHomePage> {
             const SizedBox(
               height: 50,
             ),
-            Expanded(child: CategorySection()),
+            Expanded(
+                child: FutureBuilder<List<Quiz_Category>>(
+                    future: getCategory(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        List<Quiz_Category> quiz_category = snapshot.data!;
+                        return CategorySection(quiz_category: quiz_category);
+                      } else if (snapshot.hasError) {
+                        return LottieBuilder.asset(
+                            'assets/animation/404-page-error.json');
+                      } else {}
+                      return const Center(
+                          child: CircularProgressIndicator(
+                        color: kAppBarColor,
+                      ));
+                    })),
           ],
         ),
       ),
