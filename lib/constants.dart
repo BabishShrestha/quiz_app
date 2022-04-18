2965203e9f7cae3fc20f799b7198a7a320d4878b
BabishShrestha <babishshrestha8@gmail.com>
2022-04-18 19:08:57 +0545

    Added an AppBarColor constant

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+const kAppBarColor = Colors.purple;
+
 const kHomeTextStyle = TextStyle(fontSize: 25, fontFamily: 'Poppins');
 const kCategoryTextStyle =
     TextStyle(fontSize: 20, fontFamily: 'Poppins', fontWeight: FontWeight.w700);
