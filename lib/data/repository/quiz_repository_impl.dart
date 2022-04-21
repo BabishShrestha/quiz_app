9c5e454f3fb6a2ad8d5c33cd257e435edba7caee
BabishShrestha <babishshrestha8@gmail.com>
2022-04-21 22:42:21 +0545

    Added abstraction for local data source storing

--- lib/data/repository/quiz_repository_impl.dart
+++ lib/data/repository/quiz_repository_impl.dart
@@ -1,14 +1,28 @@
+import 'package:quiz_app/data/data_sources/quiz_local_data_source.dart';
 import 'package:quiz_app/data/data_sources/quiz_remote_data_source.dart';
 import 'package:quiz_app/domain/entities/category.dart';
 import 'package:quiz_app/domain/repository/quiz_repository.dart';
 
-class QuizRepositoryImpl extends QuizRepository {
-  final CategoryRemoteDataSource categoryRemoteDataSource;
+import '../../domain/entities/questions.dart';
 
-  QuizRepositoryImpl({required this.categoryRemoteDataSource});
+class QuizRepositoryImpl extends QuizRepository {
+  final QuizRemoteDataSource quizRemoteDataSource;
+  final QuizLocalDataSource quizLocalDataSource;
+
+  QuizRepositoryImpl(
+      {required this.quizRemoteDataSource, required this.quizLocalDataSource});
 
   @override
   Future<List<Quiz_Category>> getQuizCategory() async {
-    return await categoryRemoteDataSource.getCategoryItem();
+    final remotecategory = await quizRemoteDataSource.getCategoryItem();
+    quizLocalDataSource.cacheQuizCategory(remotecategory);
+  }
+
+  @override
+  Future<List<Questions>> getQuestions(
+      Quiz_Category category, String difficulty) async {
+    final remotequestions =
+        await quizRemoteDataSource.getQuestions(category, difficulty);
+    return remotequestions;
   }
 }
