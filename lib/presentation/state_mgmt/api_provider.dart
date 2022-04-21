eb07805188949ac0251e58eee38b267b7902006d
BabishShrestha <babishshrestha8@gmail.com>
2022-04-21 22:38:21 +0545

    created a future provider method to get questions

--- lib/presentation/state_mgmt/api_provider.dart
+++ lib/presentation/state_mgmt/api_provider.dart
@@ -3,9 +3,16 @@ import 'package:http/http.dart' as http;
 
 import '../../data/data_sources/quiz_remote_data_source.dart';
 import '../../domain/entities/category.dart';
+import '../../domain/entities/questions.dart';
 
-final apiProvider = Provider<CategoryRemoteDataSourceImpl>(
-    (ref) => CategoryRemoteDataSourceImpl(client: http.Client()));
+final apiProvider = Provider<QuizRemoteDataSourceImpl>(
+    (ref) => QuizRemoteDataSourceImpl(client: http.Client()));
 
 final category_valueProvider = FutureProvider<List<Quiz_Category>>(
     (ref) async => await ref.read(apiProvider).getCategoryItem());
+FutureProvider<List<Questions>> QuestionValueProvider(
+    Quiz_Category selectedcategory, String? difficulty) {
+  return FutureProvider<List<Questions>>((ref) async => await ref
+      .read(apiProvider)
+      .getQuestions(selectedcategory, difficulty?.toLowerCase()));
+}
