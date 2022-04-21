12ec8da5b0d5f70c95efbf528b3a2b02a4a80be4
BabishShrestha <babishshrestha8@gmail.com>
2022-04-22 01:40:59 +0545

    Added Align instead of Positioned in Stack for the MaterialButton widget as an issue occured while using Positioned for the  Button. it wont be pressed in case of Positioned.

--- lib/presentation/pages/question_page.dart
+++ lib/presentation/pages/question_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:html_unescape/html_unescape.dart';
 import 'package:quiz_app/constants.dart';
+import 'package:quiz_app/presentation/pages/result_page.dart';
 
 import '../../domain/entities/questions.dart';
 
@@ -14,136 +15,114 @@ class QuestionPage extends StatefulWidget {
 }
 
 class _QuestionPageState extends State<QuestionPage> {
+  Map<int, dynamic> selectedanswers = {};
+
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
-    int currentIndex = 0;
-
+    print(selectedanswers);
     Questions question = widget.questionpool[currentIndex];
+    print(question.correctanswer);
     final List<dynamic> options = question.incorrectanswers;
-    final List<String> selectedanswers = [];
     if (!options.contains(question.correctanswer)) {
       options.add(question.correctanswer);
       options.shuffle();
     }
-    int _current = -1;
-    print(question.difficulty);
+    // var OptionColor = Colors.purple;
     return WillPopScope(
       onWillPop: _onWillPop,
       child: Scaffold(
         body: SafeArea(
-          child: SingleChildScrollView(
-            child: Stack(
-              clipBehavior: Clip.none,
-              fit: StackFit.loose,
-              children: [
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.5,
-                  alignment: Alignment.topLeft,
-                  color: Colors.purple,
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 50,
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                  colors: [
+                    const Color(0xFF3366FF),
+                    const Color(0xFF00CCFF),
+                  ],
+                )),
+                height: MediaQuery.of(context).size.height * 0.5,
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.all(10),
+                      child: Text(
+                        question.category,
+                        style: kQuestionStyle.copyWith(
+                            color: kPrimaryScaffoldColor, fontSize: 25),
                       ),
-                      Container(
-                        alignment: Alignment.centerLeft,
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          '${widget.questionpool[0].category}',
-                          style: kQuestionStyle.copyWith(
-                              color: kPrimaryScaffoldColor, fontSize: 25),
+                    ),
+                    Container(
+                      alignment: Alignment.topRight,
+                      padding: const EdgeInsets.all(10),
+                      child: const Text('0:30'),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text("Question ${(currentIndex + 1).toString()}:",
+                        style: kQuestionStyle.copyWith(
+                            color: kPrimaryScaffoldColor,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 20)),
+                  ],
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                    height: MediaQuery.of(context).size.height * 0.5,
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: kElevationToShadow[12],
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(
+                          textAlign: TextAlign.center,
+                          style: kQuestionStyle.copyWith(color: Colors.black),
+                          HtmlUnescape().convert(
+                              widget.questionpool[currentIndex].question),
                         ),
-                      ),
-                      Container(
-                        alignment: Alignment.topRight,
-                        padding: EdgeInsets.all(10),
-                        child: Text('0:30'),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Container(
-                        child: Text("${(currentIndex + 1).toString()}",
-                            style: kQuestionStyle.copyWith(
-                                color: kPrimaryScaffoldColor,
-                                fontWeight: FontWeight.w600,
-                                fontSize: 20)),
-                      ),
-                    ],
-                  ),
-                ),
-                Positioned(
-                  top: MediaQuery.of(context).size.height * 0.27,
-                  right: 20,
-                  left: 20,
-                  child: Container(
-                      height: 440,
-                      padding: EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          boxShadow: kElevationToShadow[12],
-                          borderRadius: BorderRadius.circular(20)),
-                      child: Column(
-                        children: [
-                          Text(
-                            textAlign: TextAlign.center,
-                            style: kQuestionStyle,
-                            HtmlUnescape().convert(
-                                widget.questionpool[currentIndex].question),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        ...options.map(
+                          (option) => MaterialButton(
+                            color: kAppBarColor,
+                            child: Text(HtmlUnescape().convert("$option"),
+                                style: kQuestionStyle),
+                            onPressed: () {
+                              setState(() {
+                                selectedanswers[currentIndex] = option;
+                                if (currentIndex !=
+                                    widget.questionpool.length - 1) {
+                                  currentIndex++;
+                                } else {
+                                  Navigator.of(context)
+                                      .pushReplacement(MaterialPageRoute(
+                                          builder: (context) => ResultPage(
+                                                questions: widget.questionpool,
+                                                answers: selectedanswers,
+                                              )));
+                                }
+                              });
+                            },
                           ),
-
-                          SizedBox(
-                            height: 25,
-                          ),
-
-                          ...options.map((option) => MaterialButton(
-                                color: Colors.purple,
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(20)),
-                                onPressed: () {},
-                                child: Text(
-                                  HtmlUnescape().convert(option),
-                                  style: kQuestionStyle.copyWith(
-                                    color: kPrimaryScaffoldColor,
-                                  ),
-                                ),
-                              )),
-
-                          // ListView.builder(
-                          //     itemCount: options.length,
-                          //     itemBuilder: (context, index) {
-                          //       return Card(
-                          //         elevation: 4,
-                          //         shape: RoundedRectangleBorder(
-                          //             borderRadius: BorderRadius.circular(20)),
-                          //         child: MaterialButton(
-                          //           shape: RoundedRectangleBorder(
-                          //               borderRadius: BorderRadius.circular(20)),
-                          //           onPressed: () {
-                          //             setState(() {
-                          //               _current = index;
-                          //               if (currentIndex >=
-                          //                   widget.questionpool.length) {
-                          //                 Navigator.of(context).push(
-                          //                     MaterialPageRoute(
-                          //                         builder: (context) =>
-                          //                             ResultPage()));
-                          //               } else if (currentIndex <
-                          //                   widget.questionpool.length) {
-                          //                 currentIndex++;
-                          //                 print(currentIndex);
-                          //               }
-                          //             });
-                          //           },
-                          //           child: Text('${options[index]}'),
-                          //         ),
-                          //       );
-                          //     })
-                        ],
-                      )),
-                ),
-              ],
-            ),
+                        )
+                      ],
+                    )),
+              ),
+            ],
           ),
         ),
       ),
@@ -155,18 +134,18 @@ class _QuestionPageState extends State<QuestionPage> {
           context: context,
           builder: (_) {
             return AlertDialog(
-              content: Text(
+              content: const Text(
                   "Are you sure you want to forfeit? All your progress will be lost."),
-              title: Text("Warning!"),
+              title: const Text("Warning!"),
               actions: <Widget>[
                 TextButton(
-                  child: Text("Yes"),
+                  child: const Text("Yes"),
                   onPressed: () {
                     Navigator.popUntil(context, (route) => route.isFirst);
                   },
                 ),
                 TextButton(
-                  child: Text("No"),
+                  child: const Text("No"),
                   onPressed: () {
                     Navigator.pop(context, false);
                   },
