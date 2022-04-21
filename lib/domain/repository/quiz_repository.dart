c7cb418499deb77377d645205e3fd0e45892e010
BabishShrestha <babishshrestha8@gmail.com>
2022-04-21 22:42:56 +0545

    Added abstraction for getting questions

--- lib/domain/repository/quiz_repository.dart
+++ lib/domain/repository/quiz_repository.dart
@@ -1,5 +1,9 @@
+import 'package:quiz_app/data/model/category_model.dart';
 import 'package:quiz_app/domain/entities/category.dart';
+import 'package:quiz_app/domain/entities/questions.dart';
 
 abstract class QuizRepository {
-  Future<List<Quiz_Category>> getQuizCategory();
+  Future<List<CategoryModel>> getQuizCategory();
+  Future<List<Questions>> getQuestions(
+      Quiz_Category category, String difficulty);
 }
