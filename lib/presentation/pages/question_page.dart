1b0efd075a9bf540ee21993e8dd09d340229b50d
BabishShrestha <babishshrestha8@gmail.com>
2022-04-21 22:41:35 +0545

    Modified question_page.dart to get questions from api, and display them accordingly.

--- lib/presentation/pages/question_page.dart
+++ lib/presentation/pages/question_page.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:html_unescape/html_unescape.dart';
+import 'package:quiz_app/constants.dart';
 
-class QuestionPage extends StatelessWidget {
-  const QuestionPage({Key? key}) : super(key: key);
+import '../../domain/entities/questions.dart';
+
+class QuestionPage extends StatefulWidget {
+  final List<Questions> questionpool;
+
+  const QuestionPage({required this.questionpool});
 
   @override
+  State<QuestionPage> createState() => _QuestionPageState();
+}
+
+class _QuestionPageState extends State<QuestionPage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Stack(
+    int currentIndex = 0;
+
+    Questions question = widget.questionpool[currentIndex];
+    final List<dynamic> options = question.incorrectanswers;
+    final List<String> selectedanswers = [];
+    if (!options.contains(question.correctanswer)) {
+      options.add(question.correctanswer);
+      options.shuffle();
+    }
+    int _current = -1;
+    print(question.difficulty);
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Stack(
               clipBehavior: Clip.none,
               fit: StackFit.loose,
               children: [
@@ -20,67 +42,139 @@ class QuestionPage extends StatelessWidget {
                   color: Colors.purple,
                   child: Column(
                     children: [
-                      Text('Category Name'),
                       SizedBox(
-                        height: 20,
+                        height: 50,
+                      ),
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          '${widget.questionpool[0].category}',
+                          style: kQuestionStyle.copyWith(
+                              color: kPrimaryScaffoldColor, fontSize: 25),
+                        ),
                       ),
                       Container(
                         alignment: Alignment.topRight,
+                        padding: EdgeInsets.all(10),
                         child: Text('0:30'),
                       ),
                       SizedBox(
                         height: 20,
                       ),
                       Container(
-                        child: Text('Question Count'),
-                      ),
-                      SizedBox(
-                        height: 20,
+                        child: Text("${(currentIndex + 1).toString()}",
+                            style: kQuestionStyle.copyWith(
+                                color: kPrimaryScaffoldColor,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 20)),
                       ),
                     ],
                   ),
                 ),
                 Positioned(
-                  top: MediaQuery.of(context).size.height * 0.28,
-                  right: 40,
-                  left: 40,
-                  child: Container(
-                    padding: EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(20)),
-                    child: Placeholder(),
-                  ),
-                ),
-                Positioned(
                   top: MediaQuery.of(context).size.height * 0.27,
-                  right: 30,
-                  left: 30,
-                  child: Container(
-                    padding: EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                        color: Colors.blueGrey,
-                        borderRadius: BorderRadius.circular(20)),
-                    child: Placeholder(),
-                  ),
-                ),
-                Positioned(
-                  top: MediaQuery.of(context).size.height * 0.25,
                   right: 20,
                   left: 20,
                   child: Container(
-                    padding: EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                        color: Colors.grey,
-                        borderRadius: BorderRadius.circular(20)),
-                    child: Placeholder(),
-                  ),
+                      height: 440,
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          boxShadow: kElevationToShadow[12],
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Column(
+                        children: [
+                          Text(
+                            textAlign: TextAlign.center,
+                            style: kQuestionStyle,
+                            HtmlUnescape().convert(
+                                widget.questionpool[currentIndex].question),
+                          ),
+
+                          SizedBox(
+                            height: 25,
+                          ),
+
+                          ...options.map((option) => MaterialButton(
+                                color: Colors.purple,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20)),
+                                onPressed: () {},
+                                child: Text(
+                                  HtmlUnescape().convert(option),
+                                  style: kQuestionStyle.copyWith(
+                                    color: kPrimaryScaffoldColor,
+                                  ),
+                                ),
+                              )),
+
+                          // ListView.builder(
+                          //     itemCount: options.length,
+                          //     itemBuilder: (context, index) {
+                          //       return Card(
+                          //         elevation: 4,
+                          //         shape: RoundedRectangleBorder(
+                          //             borderRadius: BorderRadius.circular(20)),
+                          //         child: MaterialButton(
+                          //           shape: RoundedRectangleBorder(
+                          //               borderRadius: BorderRadius.circular(20)),
+                          //           onPressed: () {
+                          //             setState(() {
+                          //               _current = index;
+                          //               if (currentIndex >=
+                          //                   widget.questionpool.length) {
+                          //                 Navigator.of(context).push(
+                          //                     MaterialPageRoute(
+                          //                         builder: (context) =>
+                          //                             ResultPage()));
+                          //               } else if (currentIndex <
+                          //                   widget.questionpool.length) {
+                          //                 currentIndex++;
+                          //                 print(currentIndex);
+                          //               }
+                          //             });
+                          //           },
+                          //           child: Text('${options[index]}'),
+                          //         ),
+                          //       );
+                          //     })
+                        ],
+                      )),
                 ),
               ],
             ),
-          ],
+          ),
         ),
       ),
     );
+  }
+
+  Future<bool> _onWillPop() async {
+    return (await showDialog(
+          context: context,
+          builder: (_) {
+            return AlertDialog(
+              content: Text(
+                  "Are you sure you want to forfeit? All your progress will be lost."),
+              title: Text("Warning!"),
+              actions: <Widget>[
+                TextButton(
+                  child: Text("Yes"),
+                  onPressed: () {
+                    Navigator.popUntil(context, (route) => route.isFirst);
+                  },
+                ),
+                TextButton(
+                  child: Text("No"),
+                  onPressed: () {
+                    Navigator.pop(context, false);
+                  },
+                ),
+              ],
+            );
+          },
+        )) ??
+        false;
   }
 }
