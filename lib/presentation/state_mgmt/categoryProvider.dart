93f035bd3f44c74e633feeb1c58871ba24cb2afc
BabishShrestha <babishshrestha8@gmail.com>
2022-04-22 10:22:50 +0545

    commented this provider, for future cases

--- lib/presentation/state_mgmt/categoryProvider.dart
+++ lib/presentation/state_mgmt/categoryProvider.dart
@@ -1,17 +1,17 @@
-import 'package:flutter/material.dart';
-
-class CategoryItemNotifier with ChangeNotifier {
-  int currentindex = -1;
-  void checkItem(int index) {
-    currentindex = index;
-    notifyListeners();
-  }
-
-  bool changeselectedAnimation(int index) {
-    if (currentindex == index) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-}
+// import 'package:flutter/material.dart';
+//
+// class CategoryItemNotifier with ChangeNotifier {
+//   int currentindex = -1;
+//   void checkItem(int index) {
+//     currentindex = index;
+//     notifyListeners();
+//   }
+//
+//   bool changeselectedAnimation(int index) {
+//     if (currentindex == index) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+// }
